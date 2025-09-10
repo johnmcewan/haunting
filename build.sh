660ce7fe506688c6e3706260271913d4d2f258ad
John McEwan <jmcewan3@yahoo.ca>
2025-09-10 12:47:18 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # convert our static asset files on Render
-python manage.py collectstatic --no-input
+python ./haunting/manage.py collectstatic --no-input
 
 # apply any database migrations that are outstanding
-python manage.py migrate
+python ./haunting/manage.py migrate
