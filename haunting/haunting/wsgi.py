fd9affea5ff4b1298eacdfdfab120df41a2ea307
John McEwan <jmcewan3@yahoo.ca>
2025-09-10 12:11:25 -0500

    updates

---
+++ haunting/haunting/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for haunting project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "haunting.settings")
+
+application = get_wsgi_application()
---
+++ haunting/haunting2025/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Haunting2025Config(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "haunting2025"
