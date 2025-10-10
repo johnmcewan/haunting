61512061012d01835645ce6717765dc8d27edc0d
John McEwan <jmcewan3@yahoo.ca>
2025-10-10 16:43:54 -0500

    Update settings.py

--- hlawrence/haunting2025/settings.py
+++ hlawrence/haunting2025/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!l3kw3f86#mfj2(z5q%wt+r!x2g(4g8vzbm*-7=s=!^v5q%ubz'
-#SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = 'django-insecure-!l3kw3f86#mfj2(z5q%wt+r!x2g(4g8vzbm*-7=s=!^v5q%ubz'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
